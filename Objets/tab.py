4b8c90ee7f0ab463f0f562217e81d694ba8ed3e6
lamouchedu94 <lamouchedu54@gmail.com>
2022-09-18 21:49:57 +0200

    patch

--- Objets/tab.py
+++ Objets/tab.py
@@ -5,7 +5,7 @@ class Tab_pyth :
     def __init__(self, tab = None, long = 5) :
         self.tab = [None for i in range(long)]
         self.debut = 0 
-        self.fin = 0
+        self.fin = len(self.tab)
     def est_vide(self) :
         if self.debut == self.fin :
             return True
@@ -13,6 +13,7 @@ class Tab_pyth :
     def longueur(self):
         return len(self.tab)
     def append(self,val) : 
+        assert not self.est_vide(), "tableau vide" 
         if self.fin == self.longueur():
             self.temp = [None for i in range(2*self.longueur())]
             i = 0
@@ -37,7 +38,7 @@ class Tab_pyth :
         
         
 
-test = Tab_pyth(None, 3)
+test = Tab_pyth(None, 1)
 
 print(test.longueur())
 print(test.est_vide())
@@ -46,7 +47,6 @@ test.append(2)
 test.append(3)
 print(test.tab)
 print(test.tab)
-test.pop_debut()
 test.pop_debut()
 test.pop_debut()
 test.pop_debut()
