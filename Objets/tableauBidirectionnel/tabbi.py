b7c7e57a7e6206d758fca0306cce981ab598ee4b
lamouchedu94 <lamouchedu54@gmail.com>
2022-10-06 22:05:47 +0200

    ajout tabbi

---
+++ Objets/tableauBidirectionnel/tabbi.py
@@ -0,0 +1,62 @@
+class Tabidir :
+    def __init__(self, tabg = None, tabd = None ) -> None:
+        self.tabg = []
+        if tabg != None :
+            self.tabg = tabg
+        
+        self.tabd = []
+        if tabd != None :
+            self.tabd = tabd
+    
+    def imin(self):
+        return len(self.tabg) * -1 
+
+    def imax(self) :
+        return len(self.tabd) - 1 
+
+    def append(self, val) :
+        self.tabd.append(val)
+        return None
+
+    def prepend(self, val):
+        self.tabg.append(val)
+        return None 
+    
+    def __getitem__(self,i):
+        if i < 0 :
+            return self.tabg[i*-1 -1]
+        return self.tabd[i]
+
+    def __setitem__(self, i, val):
+        if i < 0 :
+            self.tabg[i *-1 - 1 ] = val
+            return None
+        self.tabd[i] = val
+        return None
+
+    def __str__(self) :
+        res = ""
+        print(self.imin())
+        for i in range(self.imin()*-1-1, -1 , -1):
+            res += str(self.tabg[i]) + " "
+        for val in self.tabd :
+            res += str(val) + " "
+        return res
+
+    def __len__(self):
+        pass
+
+
+test = Tabidir()
+print(test)
+
+test.append(1)
+test.append(2)
+test.prepend(3)
+test.prepend(4)
+print(test[1])
+print(test[-1])
+print(test[-2])
+#test[-2] = 10
+#test[1] = 11
+print(test)
