ba9e2aaa21acffba91e51582ca6576e879d90c74
lamouchedu94 <lamouchedu54@gmail.com>
2022-10-09 17:00:01 +0200

    ajout test

--- fctrecursive/func.py
+++ fctrecursive/func.py
@@ -1,10 +1,16 @@
 def nbbits(n):
+    assert n >= 0 and type(n) == int
     if n == 0 :
         return 0
     return nbbits(n//2)+ (n & 1) 
-print(nbbits(10))
+print("Doit afficher 1 : ",nbbits(1))
+print("Doit afficher 1 : ", nbbits(2))
+print("Doit afficher 2 : ", nbbits(3))
+print("Doit afficher 2 : ", nbbits(10))
+print("Doit afficher 4 : ",nbbits(210))
 
 def syracuse(u):
+    assert u > 0
     if u == 1 :
         return 1
     if u%2 == 0 :
