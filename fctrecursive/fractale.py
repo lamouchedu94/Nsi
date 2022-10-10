db2ae642340036c0e50d22d2f0cd1056d660028d
BuildTools <unconfigured@null.spigotmc.org>
2022-10-10 19:47:41 +0200

    ajout

---
+++ fctrecursive/fractale.py
@@ -0,0 +1,36 @@
+import turtle
+
+
+#taille 100
+turtle.tracer(0, 0) 
+def fractale(n = 600):
+    #turtle.speed(0)
+    if n <= 1 :
+        turtle.forward(n)
+    else :
+        fractale(n/3)
+        turtle.left(60)
+        fractale(n/3)
+        turtle.right(120)
+        fractale(n/3)
+        turtle.left(60)
+        fractale(n/3)
+turtle.penup()
+turtle.goto([-300,500])
+turtle.pendown()
+for i in range(3):
+    fractale()
+    turtle.right(120)
+
+"""
+turtle.forward(600)
+turtle.right(120)
+turtle.forward(600)
+turtle.right(120)
+turtle.forward(600)
+turtle.right(120)
+"""
+
+turtle.update()	
+ 
+turtle.done()
--- fctrecursive/func.py
+++ fctrecursive/func.py
@@ -1,3 +1,5 @@
+import math
+ 
 def nbbits(n):
     assert n >= 0 and type(n) == int
     if n == 0 :
@@ -7,21 +9,35 @@ print("Doit afficher 1 : ",nbbits(1))
 print("Doit afficher 1 : ", nbbits(2))
 print("Doit afficher 2 : ", nbbits(3))
 print("Doit afficher 2 : ", nbbits(10))
-print("Doit afficher 4 : ",nbbits(210))
-
-def syracuse(u):
+print("Doit afficher 8 : ",nbbits(255))
+ 
+def syracuse(u,tab=None):
     assert u > 0
+    if tab == None :
+        tab = []
+    tab.append(u)
     if u == 1 :
-        return 1
+        return tab
     if u%2 == 0 :
-        u = u // 2
-    else :
-        u = 3*u +1 
-    return syracuse(u)
+        return syracuse(u//2, tab)
+    return syracuse(3*u+1, tab)
+ 
+print("\nTest 1")
 print(syracuse(10))
-
-def factorielle(n):
+print("Test 2")
+print(syracuse(4))
+ 
+def exp(x):
+    u =1 
+    for i in range(x) :
+        u = u*math.exp(-u)    
+    return u
+ 
+print(exp(100))
+ 
+def exp_r(n, u=1):
     if n == 0 :
-        return 1
-    return n*factorielle(n-1)
-#print(factorielle(21))
+        return u
+    return exp_r(n-1,u*math.exp(-u))
+ 
+print(exp_r(100))
