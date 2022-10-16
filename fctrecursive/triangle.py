b1c453ae02616a7929677b7a7b36fd031dc558fe
lamouchedu94 <lamouchedu54@gmail.com>
2022-10-16 16:12:35 +0200

    triangle dans le bon sens

--- fctrecursive/triangle.py
+++ fctrecursive/triangle.py
@@ -5,28 +5,29 @@ def triangle(l, lmin) :
         turtle.begin_fill()
         for i in range(3):
             turtle.forward(l)
-            turtle.left(120)
+            turtle.right(120)
         turtle.end_fill()
     else :
-        #
         triangle(l/2, lmin)
         turtle.forward(l/2)
         triangle(l/2, lmin)
         turtle.backward(l/2)
-        turtle.left(60)
+        turtle.right(60)
         turtle.forward(l/2)
-        turtle.right(60)
-        triangle(l/2, lmin)
         turtle.left(60)
-        turtle.backward(l/2)
+        triangle(l/2, lmin)
         turtle.right(60)
+        turtle.backward(l/2)
+        turtle.left(60)
+turtle.goto(-200, 100)
 turtle.begin_fill()
+l = 500
 for i in range(3) :
-    turtle.forward(500)
-    turtle.left(120)
+    turtle.forward(l)
+    turtle.right(120)
 turtle.end_fill()
 turtle.fillcolor("white")
-triangle(500, 3)
+triangle(l, 5)
 
 turtle.update()
 turtle.done()
