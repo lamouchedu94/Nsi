bb4aeed39a87b53ae3857594b32b70890565fe37
lamouchedu94 <lamouchedu54@gmail.com>
2022-11-08 00:08:49 +0100

    recup

--- Objets/CourseVoiture/main.py
+++ Objets/CourseVoiture/main.py
@@ -36,7 +36,25 @@ class Case:
     def __call__(self):
         return self.contenu
 
+class Carte:
+    def __init__(self, nom_fichier):
+        with open(nom_fichier) as carte:
+            self.circuit = [[Case(car) for car in ligne] for ligne in carte.readlines()]
+        return None
+    
+    def __str__(self):
+        res = ''
+        for i in range(len(self.circuit)):
+            for car in self.circuit[i] :
+                res += str(car)
+        return res
+    
+    def __repr__(self):
+        return str(self)
+    
+    def __call__(self, ligne, col):
 
+        return self.circuit[ligne][col]
 
 class Voiture : 
     def __init__(self, x_depart, y_depart):
