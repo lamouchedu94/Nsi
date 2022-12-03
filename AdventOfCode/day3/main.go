8c56078c10044226df72c348c2a2d21aced71ba6
lamouchedu94 <lamouchedu54@gmail.com>
2022-12-03 16:02:58 +0100

    day3 part2

--- AdventOfCode/day3/main.go
+++ AdventOfCode/day3/main.go
@@ -12,24 +12,58 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	calc(tab)
+	fmt.Println("Partie 1 :", part1(tab))
+	fmt.Println("Partie 2 :", part2(tab))
+	//A = 65
+	//a = 97
 }
 
-func calc(tab []string) {
-	var sep int
-	for _, car := range tab {
-		sep = len(car) / 2
-
-		for i := 0; i < sep; i++ {
-			for j := sep; j < len(car); j++ {
-				if car[i] == car[j] {
-					fmt.Println(string(car[i]), car[j])
+func part2(tab []string) int {
+	//var sep int
+	var res int
+	for i := 0; i < len(tab); i += 3 {
+	out:
+		for j := 0; j < len(tab[i]); j++ {
+			for k := 0; k < len(tab[i+1]); k++ {
+				for l := 0; l < len(tab[i+2]); l++ {
+					if tab[i][j] == tab[i+1][k] && tab[i][j] == tab[i+2][l] {
+						if 65 <= int(tab[i][j]) && int(tab[i][j]) <= 90 {
+							res += int(tab[i][j]) - 38
+						} else {
+							res += int(tab[i][j]) - 96
+						}
+						break out
+						//fmt.Println(tab[i][j])
+					}
 				}
 			}
 		}
-		break
 	}
+	return res
+}
 
+func part1(tab []string) int {
+	var sep int
+	var res int
+	for _, car := range tab {
+		sep = len(car) / 2
+	out:
+		for i := 0; i < sep; i++ {
+			for j := sep; j < len(car); j++ {
+				if car[i] == car[j] {
+					if 65 <= int(car[i]) && int(car[i]) <= 90 {
+						res += int(car[i]) - 38
+					} else {
+						res += int(car[i]) - 96
+					}
+					break out
+				}
+
+			}
+		}
+
+	}
+	return res
 }
 
 func read(path string) ([]string, error) {
@@ -43,3 +77,9 @@ func read(path string) ([]string, error) {
 	}
 	return tab, err
 }
+
+/*
+vJrwpWtwJgWr 	 hcsFMMfFFhFp
+jqHRNqRjqzjGDLGL rsFMfFZSrLrFZsSL
+PmmdzqPrV		 vPwwTWBwg
+*/
