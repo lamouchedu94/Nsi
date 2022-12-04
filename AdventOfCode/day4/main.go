82f610d9469f2f9798f76795f2ac51e97b30b1e3
lamouchedu94 <lamouchedu54@gmail.com>
2022-12-04 22:10:43 +0100

    Day4 part2

--- AdventOfCode/day4/main.go
+++ AdventOfCode/day4/main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main() {
-	tab, _ := read("input.txt")
+	tab, _ := read("input1.txt")
 	//fmt.Println(temp[0])
 	//fmt.Printf("%s and %s", tab[0], tab[1])
-	fmt.Println(run(tab))
+	fmt.Println(run1(tab))
+	fmt.Println(run2(tab))
 }
 
 // [2 4]
@@ -23,7 +24,21 @@ type team struct {
 	a, b, c, d int
 }
 
-func run(tab []team) int {
+func run2(tab []team) int {
+	var res int
+
+	for _, t := range tab {
+		if (t.a <= t.c && t.c <= t.b) || (t.c <= t.a && t.a <= t.d) {
+			res += 1
+		}
+
+		//fmt.Println("\n", grp1, grp2)
+	}
+
+	return res
+}
+
+func run1(tab []team) int {
 	var res int
 
 	for _, t := range tab {
