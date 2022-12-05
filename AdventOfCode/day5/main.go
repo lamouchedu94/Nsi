b76740bb5dfd0097bc14cba3e6298148bed4cf4f
lamouchedu94 <lamouchedu54@gmail.com>
2022-12-05 23:07:06 +0100

    Part1 + Part2

--- AdventOfCode/day5/main.go
+++ AdventOfCode/day5/main.go
@@ -7,126 +7,115 @@ import (
 )
 
 func main() {
-	tab, tabinstruc, err := read("input.txt")
-	_ = tabinstruc
+	j, err := read("input1.txt", 9)
+	//_ = tabinstruc
 	if err != nil {
 		fmt.Println(err)
 	}
+	run1(j)
+	fmt.Println()
+	j, err = read("input1.txt", 9)
+	//_ = tabinstruc
+	if err != nil {
+		fmt.Println(err)
+	}
+	run2(j)
 
-	run(tab, tabinstruc)
-	//fmt.Println(tab[1])
-	//fmt.Println(tabinstruc)
 }
 
+type jeux struct {
+	quai         []pile
+	instructions []instruct
+}
+
+type pile []byte
 type instruct struct {
-	a, b, c, d, e, f, g, h, i, j int
+	quantite, de, vers int
 }
 
-// DNZ
-// CM
-// P DNZ
-
-func run(tab []string, tabinstruc []instruct) {
-	for _, t := range tabinstruc {
-		for i := 0; i < t.a; i++ {
-			//bouge depuis le 2 de 0
-			var abouger string
-			if len(string(tab[t.b-1])) > 0 {
-				abouger = string(tab[t.b-1][0])
+func run1(j jeux) {
+	for _, inst := range j.instructions {
+		for i := 0; i < inst.quantite; i++ {
+			abouger := j.quai[inst.de][0]
+			j.quai[inst.de] = j.quai[inst.de][1:]
+			j.quai[inst.vers] = append(pile{abouger}, j.quai[inst.vers]...)
+		}
+		/*
+			for _, q := range j.quai {
+				fmt.Println(string(q))
 			}
-
-			//valeur de la pile 1 en supprimant le truc le plus haut si espace
-			var temp string
-			if len(string(tab[t.c-1])) > 0 {
-				if string(tab[t.c-1][0]) == " " {
-					var ind int
-					for _, car := range tab[t.c-1] {
-						if string(car) == " " {
-							ind += 1
-						}
-					}
-					temp = tab[t.c-1][ind:len(tab[t.c-1])]
-
-				} else {
-					temp = tab[t.c-1]
-				}
-			}
-			tab[t.c-1] = abouger + temp
-			tab[t.b-1] = tab[t.b-1][1:len(tab[t.b-1])]
-
-		}
-		fmt.Println(tab)
+			fmt.Println()
+		*/
 	}
-
+	fmt.Print("Part 1 : ")
+	for _, q := range j.quai {
+		_ = q
+		fmt.Print(string(q[0]))
+	}
 }
 
-func decoupage(line string, tab []string) []string {
-	j := 0
-	for i := 1; i < 12; i += 4 {
-		if string(line[i]) != " " {
-			tab[j] += string(line[i])
-		} else {
-			tab[j] += " "
-		}
-		j += 1
-	}
-	return tab
-	/*
-		if string(line[1]) != " " {
-			tab[0] += string(line[1])
-		} else {
-			tab[0] += " "
-		}
-		if string(line[5]) != " " {
-			tab[1] += string(line[5])
-		} else {
-			tab[1] += " "
-		}
-		if string(line[9]) != " " {
-			tab[2] += string(line[9])
-		} else {
-			tab[2] += " "
-		}
-		return tab
-	*/
-}
-func read(path string) ([]string, []instruct, error) {
-	var tab []string
-	var tabinstruction []instruct
-	fin := true
+func run2(j jeux) {
+	for _, inst := range j.instructions {
+		abouger := make(pile, inst.quantite)
+		copy(abouger, j.quai[inst.de][0:inst.quantite])
+		j.quai[inst.de] = j.quai[inst.de][inst.quantite:]
+		j.quai[inst.vers] = append(abouger, j.quai[inst.vers]...)
 
-	//var colonne []string
-	for i := 0; i < 3; i++ {
-		temp := []string{""}
-		tab = append(tab, temp...)
+	}
+	fmt.Print("Part 2 : ")
+	for _, q := range j.quai {
+		_ = q
+		fmt.Print(string(q[0]))
+	}
+}
+
+func read(path string, stacks int) (jeux, error) {
+	var j jeux
+
+	//initiliser le jeux
+	for i := 0; i < stacks; i++ {
+		j.quai = append(j.quai, pile{})
 	}
 
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, nil, err
+		return j, err
 	}
 	defer file.Close()
 	test := bufio.NewScanner(file)
 	test.Split(bufio.ScanLines)
+out:
 	for test.Scan() {
-
-		if test.Text() == " 1   2   3 " {
-			fin = false
-
-		}
-		if fin {
-			tab = decoupage(test.Text(), tab)
-			//tab = append(tab, test.Text())
-		} else {
-			line := test.Text()
-			m := instruct{}
-			fmt.Sscanf(line, "move %d from %d to %d", &m.a, &m.b, &m.c, &m.d, &m.e, &m.f, &m.g, &m.h, &m.i, &m.j)
-			if m.a != 0 {
-				tabinstruction = append(tabinstruction, m)
+		ligne := test.Bytes()
+		for c := 0; c <= stacks; c++ {
+			pos := 4*c + 1
+			if pos < len(ligne) {
+				if ligne[pos] == '1' {
+					break out
+				}
+				if ligne[pos] != ' ' {
+					j.quai[c] = append(j.quai[c], ligne[pos])
+				}
 			}
-
 		}
 
 	}
-	return tab, tabinstruction, err
+	if test.Err() != nil {
+		return j, test.Err()
+	}
+	for test.Scan() {
+		ligne := test.Text()
+		if ligne == "" {
+			continue
+		}
+		m := instruct{}
+		fmt.Sscanf(ligne, "move %d from %d to %d", &m.quantite, &m.de, &m.vers)
+		if m.quantite != 0 {
+			m.de--
+			m.vers--
+			j.instructions = append(j.instructions, m)
+		}
+
+	}
+	return j, nil
 }
