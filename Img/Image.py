9fd3a70564997457e6aafa5aeac16e84f8255d78
lamouchedu94 <lamouchedu54@gmail.com>
2023-02-16 22:15:12 +0100

    ?

--- Img/Image.py
+++ Img/Image.py
@@ -97,14 +97,12 @@ def rotation(photo):
 new = rotation(photo)
 print()
 affichage(new)
-
-
 '''
 from PIL import Image
 photo=Image.open('Img/photo.bmp')
- 
- 
- 
+import time 
+
+
 def rotation_indirecte(img):
     hauteur,largeur=photo.size
     tourne=Image.new(mode='RGB',size=(hauteur,largeur))
@@ -142,9 +140,67 @@ def rotation_directe_sp(img):
             photo.putpixel((largeur-1-colonne,ligne),photo.getpixel((hauteur-1-ligne, largeur-1-colonne)))
             photo.putpixel((hauteur-1-ligne, largeur-1-colonne),photo.getpixel((colonne,hauteur-1-ligne)))
             photo.putpixel((colonne,hauteur-1-ligne),temp)
+    
+    #img.show()
     return None
  
 #photo.show()
 #rotation_indirecte_sp(photo)
-rotation_directe_sp(photo)
-photo.show()
+#rotation_directe_sp(photo)
+#photo.show()
+
+def rota_recurs(img):
+    h = img.size[0]//2
+    if h>=1:
+        a = rota_recurs(img.crop((0, 0, h, h)))
+        b = rota_recurs(img.crop((0, h, h, 2*h)))
+        c = rota_recurs(img.crop((h,0,2*h,h)))
+        d = rota_recurs(img.crop((h,h,2*h,2*h)))
+        img.paste(a,(h, 0))
+        img.paste(b,(0,0))
+        img.paste(c,(h,h))
+        img.paste(d,(0,h))
+    return img
+    
+def rotation_dire_recu_sp(img, h=0,g=0, taille= None):
+    if taille==None :
+        taille=img.size[0]
+    if taille>= 1 :
+        k=taille//2
+        rotation_dire_recu_sp(img,h,g,k)
+        rotation_dire_recu_sp(img,h,g+k,k)
+        rotation_dire_recu_sp(img,h+k,g,k)
+        rotation_dire_recu_sp(img,h+k,g+k,k)
+        permutation_indirecte(img,h,g,taille)
+    return None
+#rota_recurs(photo).show()
+
+def permutation_indirecte(img,h,g,taille):
+    k = taille//2
+    temp = img.crop((h,g,k+h,g+k))
+    img.paste(img.crop((h,g+k,h+k, g+2*k)), (h,g))
+    img.paste(img.crop((h,g+k,h+2*k, g+2*k)), (h,g+k))
+    img.paste(img.crop((h+k,g+k,h+2*k, g+2*k)), (h,g+k))
+    img.paste(temp, (h+k,g))
+    return None
+    
+def permutation_directe(img,h,g,taille):
+    pass
+
+"""
+def f():
+    for i in range(100):
+        #rotation_directe_sp(photo)
+        rota_recurs(photo)
+    
+    
+    #time.sleep(10)
+
+f()
+"""
+"""
+print(process.memory_info().rss)
+mem_usage = memory_usage(f)
+print('Memory usage (in chunks of .1 seconds): %s' % mem_usage)
+print('Maximum memory usage: %s' % max(mem_usage))
+"""
