ff11654675ce31b2807944809bdf3a5850d29832
lamouchedu94 <lamouchedu54@gmail.com>
2023-03-28 17:21:32 +0200

    bac sujet

---
+++ Bac/ex20.py
@@ -0,0 +1,36 @@
+def ajoute_dictionnaires(d1, d2) :
+    """
+    Paramètres : Deux dictionnaires dont les clés sont des nombres
+    Revoi : La fusion des deux dictionnaires     
+    """
+    res = {}
+    for val in d1 :
+        res[val] = d1[val]
+        if val in d2 :
+            res[val] += d2[val]
+    for val in d2 :
+        if val not in res :
+            res[val] = d2[val]
+    return res
+    
+
+assert ajoute_dictionnaires({1: 5, 2: 7}, {2: 9, 3: 11}) == {1: 5, 2: 16, 3: 11}
+assert ajoute_dictionnaires({}, {2: 9, 3: 11}) == {2: 9, 3: 11} 
+assert ajoute_dictionnaires({2: 9, 3: 11}, {}) == {2: 9, 3: 11}
+assert ajoute_dictionnaires({1: 5, 2: 7}, {2 : 2}) == {1: 5, 2: 9}
+
+from random import randint
+def nbre_coups():
+    n = 0
+    cases_vues = [0]
+    case_en_cours = 0
+    nbre_cases = 12
+    while len(cases_vues) < nbre_cases:
+        x = randint(1, 6)
+        case_en_cours = (case_en_cours + x) % 12
+        if case_en_cours not in cases_vues :
+            cases_vues.append(case_en_cours)
+        n = n + 1 
+    return n
+
+print(nbre_coups())
