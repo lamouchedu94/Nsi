d2df4ab966e4e804a3581a80a681efc1c0d56b61
lamouchedu94 <lamouchedu54@gmail.com>
2023-04-13 22:44:45 +0200

    ça marche

--- Graph/morpion.py
+++ Graph/morpion.py
@@ -40,14 +40,54 @@ def verif(tab) :
         for j in range(3) :
             if tab[i][j] == 1 :
                 count += 1
-        
-            
-
-
+        if count == 3 :
+            return (True, 1)
+        count = 0 
+    
+    count = 0
+    for i in range(3):
+        for j in range(3) :
+            if tab[i][j] == 2 :
+                count += 1 
+        if count == 3 :
+            return (True, 2)
+        count = 0
+    
     #cas diagonales 
-    # polo est un petit caca    
+    #polo est un petit caca    
 
+    count = 0
+    for i in range(3) :
+        if tab[i][i] == 1 :
+            count += 1 
+    if count == 3 :
+        return (True, 1)
+    
+    count = 0
+    for i in range(3) :
+        if tab[i][i] == 2 :
+            count += 1 
+    if count == 3 :
+        return (True, 2)
 
+    count = 0
+    ind = 0 
+    for i in range(2,-1,-1) :
+        if tab[i][ind] == 1 :
+            count += 1
+        ind += 1 
+    if count == 3 :
+        return (True, 1)
+
+    count = 0
+    ind = 0 
+    for i in range(2,-1,-1) :
+        if tab[i][ind] == 2 :
+            count += 1
+        ind += 1 
+    if count == 3 :
+        return (True, 2)
+    
     return (False,0) 
 
 while run:
@@ -59,7 +99,7 @@ while run:
                 pos=pygame.mouse.get_pos()
                 case=(pos[0]//larg_case,pos[1]//larg_case)
                 #print(case)
-                print(tab)
+                
                 if tab[case[1]][case[0]]==0:
                     if joueur==1:
                         pygame.draw.circle(surf,couleur_ronds,(case[0]*larg_case+larg_case//2,case[1]*larg_case+larg_case//2),(int(0.475*larg_case)),4)
@@ -78,8 +118,10 @@ while run:
                     couleur = couleur_ronds
                 else :
                     couleur = couleur_croix
+                
                 text_surface = font.render(f"le joueur {val_verif[1]} a gagné ! whaouuuu trop fort bg tu gères", False, couleur)
-                surf.blit(text_surface, ((larg_case*2)//2,(larg_case*2)//2))
+                text_x,text_y = text_surface.get_size()
+                surf.blit(text_surface, (((larg_case*3)//2)-text_x//2,((larg_case*3)//2)-text_y//2))
                 pygame.display.flip()
                 time.sleep(3)
                 run = False
